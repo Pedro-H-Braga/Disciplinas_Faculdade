c9f11fa38a483352f2852f957d8f7c5a8eb3ca19
Pedro Braga <pedrobragalima@gmail.com>
2023-07-10 21:20:49 -0300

    update

---
+++ Disciplina-Prog-Redes-2023.1/Avaliações/cliente_servidor_telegram/Scripts/const.py
@@ -0,0 +1,6 @@
+# ---------CONSTANTES-----------------
+SERVER      = 'localhost'
+PORT        = 5678
+BUFFER_MSG  = 1024
+# mensagem padrão do prompt para inputs
+PROMPT = 'Digite sua msg (!q para terminar) > '
