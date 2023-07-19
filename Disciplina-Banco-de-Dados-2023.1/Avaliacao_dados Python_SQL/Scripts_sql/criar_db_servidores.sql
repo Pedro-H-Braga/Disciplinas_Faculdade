5531fed36691e8028b1afc8f5b340b2c438f6bc8
Pedro Braga <pedrobragalima@gmail.com>
2023-07-19 11:22:21 -0300

    update

---
+++ Disciplina-Banco-de-Dados-2023.1/Avaliacao_dados Python_SQL/Scripts_sql/criar_db_servidores.sql
@@ -0,0 +1,6 @@
+--------------------------------
+-- Criando database servidores
+
+create database servidores;
+--------------------------------
+-- como será usado o schema public, não será necessário criar um. Pois já é por padrão
--- Disciplina-Banco-de-Dados-2023.1/Avaliacao_dados Python_SQL/Scripts_sql/criar_estrutura_db.sql
+++ Disciplina-Banco-de-Dados-2023.1/Avaliacao_dados Python_SQL/Scripts_sql/criar_estrutura_db.sql
@@ -1,71 +1,86 @@
-CREATE TABLE "setor"(
-    "nome_setor" VARCHAR(n) NOT NULL,
-    "id_setor" SERIAL NOT NULL
+-- ------------------------------
+-- criando tabela SETOR
+CREATE TABLE setor(
+    nome_setor VARCHAR() NOT NULL,
+    id_setor SERIAL NOT NULL
+    CONSTRAINT pk_setor PRIMARY KEY(id_setor)
 );
-ALTER TABLE
-    "setor" ADD PRIMARY KEY("id_setor");
-CREATE TABLE "campus"(
-    "campi" VARCHAR(10) NOT NULL,
-    "id_campus" SERIAL NOT NULL
+
+-- ------------------------------
+-- criando tabela FUNCAO
+CREATE TABLE funcao(
+    funcao_servidor VARCHAR() NULL,
+    id_funcao SERIAL NOT NULL
+	CONSTRAINT pk_funcao PRIMARY KEY(id_funcao)
 );
-ALTER TABLE
-    "campus" ADD PRIMARY KEY("id_campus");
-CREATE TABLE "funcao"(
-    "funcao_servidor" VARCHAR(n) NULL,
-    "id_funcao" SERIAL NOT NULL
+
+-- ------------------------------
+-- criando tabela DISCIPLINAS
+CREATE TABLE disciplina_ingresso(
+    disciplina VARCHAR() NULL,
+    id_disciplina SERIAL NOT NULL
+	CONSTRAINT pk_disciplina_ingresso PRIMARY KEY(id_disciplina)
 );
-ALTER TABLE
-    "funcao" ADD PRIMARY KEY("id_funcao");
-CREATE TABLE "disciplina_ingresso"(
-    "disciplina" VARCHAR(n) NULL,
-    "id_disciplina" SERIAL NOT NULL
+
+-- ------------------------------
+-- criando tabela JORNADA
+CREATE TABLE jornada_trabalho(
+    jornadaTrabalho VARCHAR(100) NOT NULL,
+    id_jornada SERIAL NOT NULL
+	CONSTRAINT pk_jornada_trabalho PRIMARY KEY(id_jornada)
 );
-ALTER TABLE
-    "disciplina_ingresso" ADD PRIMARY KEY("id_disciplina");
-CREATE TABLE "jornada_trabalho"(
-    "jornadaTrabalho" VARCHAR(n) NOT NULL,
-    "id_jornada" SERIAL NOT NULL
+
+-- ------------------------------
+-- criando tabela TELEFONES
+CREATE TABLE telefones_institucionais(
+    telefone CHAR(11) NULL,
+    ramal CHAR(4) NOT NULL,
+    id_telefones SERIAL NOT NULL
+    CONSTRAINT pk_telefones_institucionais PRIMARY KEY(id_telefones)
 );
-ALTER TABLE
-    "jornada_trabalho" ADD PRIMARY KEY("id_jornada");
-CREATE TABLE "telefones_institucionais"(
-    "telefone" CHAR(11) NULL,
-    "ramal" CHAR(n) NOT NULL,
-    "id_telefones" SERIAL NOT NULL
+
+-- ------------------------------
+-- Criando a tabela CAMPI
+create table campi(
+	campus			varchar(10),
+	nome_completo	varchar(50),
+	constraint pk_campi primary key (campus)
 );
-ALTER TABLE
-    "telefones_institucionais" ADD PRIMARY KEY("id_telefones");
-CREATE TABLE "servidor"(
-    "matricula" INTEGER NOT NULL,
-    "categoria" VARCHAR(n) NOT NULL,
-    "cargo" VARCHAR(n) NULL,
-    "nome" VARCHAR(n) NOT NULL,
-    "curriculoLattes" VARCHAR(n) NULL,
-    "urlFoto75x100" VARCHAR(n) NOT NULL,
-    "id_setor" SERIAL NULL,
-    "id_disciplina" SERIAL NULL,
-    "id_funcao" SERIAL NULL,
-    "id_jornada" SERIAL NULL,
-    "id_telefones" SERIAL NULL,
-    "id_campus" SERIAL NULL
+
+-- ------------------------------
+-- criando tabela principal de SERVIDORES
+CREATE TABLE servidor(
+    matricula INTEGER NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    cargo VARCHAR() NULL,
+    nome VARCHAR() NOT NULL,
+    curriculoLattes VARCHAR() NULL,
+    urlFoto75x100 VARCHAR() NOT NULL,
+    id_setor SERIAL,
+    id_disciplina SERIAL,
+    id_funcao SERIAL,
+    id_jornada SERIAL,
+    id_telefones SERIAL,
+    id_campus SERIAL,
+	
+	CONSTRAINT pk_servidor ADD PRIMARY KEY(matricula),
+	------------------------------------------------------------------
+	-- importando as FK das tabelas externas
+	CONSTRAINT servidor_id_telefones_foreign FOREIGN KEY(id_telefones) 
+				REFERENCES telefones_institucionais(id_telefones),
+	
+	CONSTRAINT servidor_id_setor_foreign FOREIGN KEY(id_setor) 
+				REFERENCES setor(id_setor),
+	
+	CONSTRAINT servidor_id_disciplina_foreign FOREIGN KEY(id_disciplina) 
+				REFERENCES disciplina_ingresso(id_disciplina),
+	
+	CONSTRAINT servidor_id_campus_foreign FOREIGN KEY(id_campus) 
+				REFERENCES campus(id_campus),
+	
+	CONSTRAINT servidor_id_jornada_foreign FOREIGN KEY(id_jornada) 
+				REFERENCES jornada_trabalho(id_jornada),
+	
+	CONSTRAINT servidor_id_funcao_foreign FOREIGN KEY(id_funcao) 
+				REFERENCES funcao(id_funcao)
 );
-ALTER TABLE
-    "servidor" ADD CONSTRAINT "servidor_nome_curriculolattes_urlfoto75x100_unique" UNIQUE(
-        "nome",
-        "curriculoLattes",
-        "urlFoto75x100"
-    );
-ALTER TABLE
-    "servidor" ADD PRIMARY KEY("matricula");
-ALTER TABLE
-    "servidor" ADD CONSTRAINT "servidor_id_telefones_foreign" FOREIGN KEY("id_telefones") REFERENCES "telefones_institucionais"("id_telefones");
-ALTER TABLE
-    "servidor" ADD CONSTRAINT "servidor_id_setor_foreign" FOREIGN KEY("id_setor") REFERENCES "setor"("id_setor");
-ALTER TABLE
-    "servidor" ADD CONSTRAINT "servidor_id_disciplina_foreign" FOREIGN KEY("id_disciplina") REFERENCES "disciplina_ingresso"("id_disciplina");
-ALTER TABLE
-    "servidor" ADD CONSTRAINT "servidor_id_campus_foreign" FOREIGN KEY("id_campus") REFERENCES "campus"("id_campus");
-ALTER TABLE
-    "servidor" ADD CONSTRAINT "servidor_id_jornada_foreign" FOREIGN KEY("id_jornada") REFERENCES "jornada_trabalho"("id_jornada");
-ALTER TABLE
-    "servidor" ADD CONSTRAINT "servidor_id_funcao_foreign" FOREIGN KEY("id_funcao") REFERENCES "funcao"("id_funcao");
