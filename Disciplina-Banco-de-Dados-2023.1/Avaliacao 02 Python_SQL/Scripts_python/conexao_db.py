29e80eb112d129c4de42189c396f1a844d4fded9
Pedro Braga <pedrobragalima@gmail.com>
2023-07-20 21:40:14 -0300

    update

--- Disciplina-Banco-de-Dados-2023.1/Avaliacao 02 Python_SQL/Scripts_python/conexao_db.py
+++ Disciplina-Banco-de-Dados-2023.1/Avaliacao 02 Python_SQL/Scripts_python/conexao_db.py
@@ -3,4 +3,4 @@
 DB_HOST = 'localhost'       # Host onde o DB está armazenado
 DB_NAME = 'servidores'      # Database a ser conectado
 DB_USER = 'postgres'        # User a ser utilizado na conexão
-DB_PASS = 'admin'           # Password do User
+DB_PASS = 'postgres'           # Password do User
