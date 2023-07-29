89cc4070df34ae86eab1cfbe05c74773a59fa173
Pedro Braga <pedrobragalima@gmail.com>
2023-07-29 13:06:23 -0300

    udpate

--- Disciplina-Prog-Redes-2023.1/Avaliações/cliente_servidor_telegram/Scripts/const.py
+++ Disciplina-Prog-Redes-2023.1/Avaliações/cliente_servidor_telegram/Scripts/const.py
@@ -10,17 +10,17 @@ COMAND_ERROR = '\
 ----------------------------------------------------------------------------------------------------------\
 \tCOMANDOS DISPONÍVEIS:\
 ----------------------------------------------------------------------------------------------------------\
-i.    /q → sair do cliente;\n \
-ii.   /l → listar o IP:porta dos clientes conectados no servidor;\n \
-iii.  /m:ip_destino:porta:mensagem → Enviar uma mensagem a um determinado cliente conectado no servidor\n \
-iv.   /b:mensagem → Enviar uma mensagem para todos os clientes conectados no servidor\n \
-v.    /h → listar as mensagens já enviadas ao servidor pelo usuário (histórico);\n \
-vi.   /f → listar os arquivos (nome e tamanho) contidos na pasta /server_files (do servidor);\n \
-vii.  /d:nome_arquivo → efetuar o “download” do arquivo especificado para a pasta /download (do cliente);\n \
-viii. /u:nome_arquivo →efetuar o “upload” de um arquivo para a pasta /server_files (do servidor);\n \
-ix.   /w:url →efetuar o download do arquivo fornecido na url para a pasta /server_files (do servidor);\n \
-x.    /rss:palavra_chave →listar as 10 notícias mais recentes que contenham a palavra_chave. Deverá ser habilitado pelo menos 10 URLs que forneçam conteúdo em formato RSS;\n \
-xi.   /? → exibir uma ajuda (listar as opções contidas nesse roteiro).'
+/q → sair do cliente;\n \
+/l → listar o IP:porta dos clientes conectados no servidor;\n \
+/m:ip_destino:porta:mensagem → Enviar uma mensagem a um determinado cliente conectado no servidor\n \
+/b:mensagem → Enviar uma mensagem para todos os clientes conectados no servidor\n \
+/h → listar as mensagens já enviadas ao servidor pelo usuário (histórico);\n \
+/f → listar os arquivos (nome e tamanho) contidos na pasta /server_files (do servidor);\n \
+/d:nome_arquivo → efetuar o “download” do arquivo especificado para a pasta /download (do cliente);\n \
+/u:nome_arquivo →efetuar o “upload” de um arquivo para a pasta /server_files (do servidor);\n \
+/w:url →efetuar o download do arquivo fornecido na url para a pasta /server_files (do servidor);\n \
+/rss:palavra_chave →listar as 10 notícias mais recentes que contenham a palavra_chave. Deverá ser habilitado pelo menos 10 URLs que forneçam conteúdo em formato RSS;\n \
+/? → exibir uma ajuda (listar as opções contidas nesse roteiro).'
 # --------- VARIAVEIS SOCKET -----------------
 allSocks = []
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- Disciplina-Prog-Redes-2023.1/Avaliações/cliente_servidor_telegram/Scripts/funcs_comands.py
+++ Disciplina-Prog-Redes-2023.1/Avaliações/cliente_servidor_telegram/Scripts/funcs_comands.py
@@ -1,5 +1,6 @@
 # ----------------- FUNÇÕES FUNCIONALIDADES -----------------
 # Funções das opções do servidor em clientInterection
+from const import *
 
 # listar o IP:porta dos clientes conectados no servidor;
 def l(msg, addrSource):
@@ -9,39 +10,6 @@ def l(msg, addrSource):
     for sockConn, addr in allSocks:
         if addr != addrSource:
             sockConn.send(msg.encode(CODE_PAGE))
-
-
-# Enviar uma mensagem a um determinado cliente conectado no servidor
-def m(msg, sock):
-    # /m:ip_destino:porta:mensagem
-    msg_str = msg.decode(CODE_PAGE)
-
-    try:
-        # pegando os dados do /m
-        split_list = msg_str.split(':')
-        ip_dest = split_list[1]
-        port = split_list[2]
-        msg_dest = split_list[3]
-    except Exception as e:
-        msg = 'O comando deve estar no seguinte formato: /m:ip_destino:porta:mensagem'
-        sockConn.send(msg.encode(CODE_PAGE))
-        print(f'ERROR: {e}')
-
-    # enviar msg_dest para o ip/port informado
-    print (f"Enviando -> {msg_dest}\npara: {ip_dest} | {port}")
-    sock.bind((ip_dest, port))
-    sock.listen(1)
-    sockConn, addres = sock.accept()
-    addrSource = ((sockConn, addres))
-    print('Connect a: ', addrSource)
-
-    # verificar se existe em allSocks
-    if addrSource in allSocks:
-        # envia a mensagem
-        sockConn.send(msg_dest.encode(CODE_PAGE))
-    else:
-        msg_error = 'Cliente NÃO está conectado ao servidor! Tente outro ip | porta'
-        sockConn.send(msg_error.encode(CODE_PAGE))
 
 
 # /b:mensagem → Enviar uma mensagem para todos os clientes conectados no servidor
