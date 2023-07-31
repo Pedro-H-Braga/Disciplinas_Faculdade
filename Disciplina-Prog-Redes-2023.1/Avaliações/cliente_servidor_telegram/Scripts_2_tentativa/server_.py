e3cea1fea7180b9ffba321754fbea2ad9cb1b307
Pedro Braga <pedrobragalima@gmail.com>
2023-07-31 12:07:48 -0300

    update

--- Disciplina-Prog-Redes-2023.1/Avaliações/cliente_servidor_telegram/Scripts_2_tentativa/server_.py
+++ Disciplina-Prog-Redes-2023.1/Avaliações/cliente_servidor_telegram/Scripts_2_tentativa/server_.py
@@ -4,7 +4,7 @@ from constantes import *
 # incializando o historico
 message_history = {}
 listMessage     = ''
-# sockConn = objeto de conexão socket do cliente
+# sockConn = objeto de conexão socket do cliente (utilizando para enviar os dados para cada cliente)
 # addr = address (IP/PORTA) do cliente
 def cliInteraction(sockConn, addr):
     # inciando msg binario como vazia
